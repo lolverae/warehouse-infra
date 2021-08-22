e1f3eb0ee86205f4b19aeb374fe4dffa8592c37a
lolverae <lolverae1300@alumno.ipn.mx>
2021-08-22 13:01:27 -0500

    just started from scratch lmao

--- centos-main.tf
+++ centos-main.tf
@@ -1,71 +1,62 @@
-terraform {
-  required_providers {
-    azurerm = {
-      source  = "hashicorp/azurerm"
-      version = "~> 2.65"
-    }
-  }
-  required_version = ">= 0.14.9"
-}
-
 provider "azurerm" {
   features {}
 }
 
-resource "azurerm_resource_group" "warehouse-rg" {
-  name     = "warehouse-project"
-  location = "westus2"
+resource "azurerm_resource_group" "warehouse-service" {
+  name     = "warehouse-service-resources"
+  location = "West Europe"
 }
 
-resource "azurerm_virtual_network" "warehouse-vnet" {
-    name                = "warehouse-net"
-    address_space       = ["10.0.0.0/16"]
-    location            = "westus2"
-    resource_group_name = azurerm_resource_group.warehouse-rg.name
+resource "azurerm_virtual_network" "warehouse-service" {
+  name                = "warehouse-service-network"
+  address_space       = ["10.0.0.0/16"]
+  location            = azurerm_resource_group.warehouse-service.location
+  resource_group_name = azurerm_resource_group.warehouse-service.name
 }
-resource "azurerm_network_interface" "warehouse-nic" {
-  name                = "warehouse-interface"
-  location            = "westus2"
-  resource_group_name = azurerm_resource_group.warehouse-rg.name
+
+resource "azurerm_subnet" "warehouse-service" {
+  name                 = "internal"
+  resource_group_name  = azurerm_resource_group.warehouse-service.name
+  virtual_network_name = azurerm_virtual_network.warehouse-service.name
+  address_prefixes     = ["10.0.2.0/24"]
+}
+
+resource "azurerm_network_interface" "warehouse-service" {
+  name                = "warehouse-service-nic"
+  location            = azurerm_resource_group.warehouse-service.location
+  resource_group_name = azurerm_resource_group.warehouse-service.name
 
   ip_configuration {
     name                          = "internal"
-    subnet_id                     = azurerm_subnet.example.id
+    subnet_id                     = azurerm_subnet.warehouse-service.id
     private_ip_address_allocation = "Dynamic"
   }
 }
 
-# Create virtual machine
-resource "azurerm_linux_virtual_machine" "ansible-node" {
-    name                  = "ansible-node1"
-    location              = "westus2"
-    resource_group_name   = azurerm_resource_group.warehouse-rg.name
-    network_interface_ids = [azurerm_network_interface.warehouse-vnet.id]
-    size                  = "Standard_DS1_v2"
+resource "azurerm_linux_virtual_machine" "warehouse-service" {
+  name                = "warehouse-service-machine"
+  resource_group_name = azurerm_resource_group.warehouse-service.name
+  location            = azurerm_resource_group.warehouse-service.location
+  size                = "Standard_F2"
+  admin_username      = "adminuser"
+  network_interface_ids = [
+    azurerm_network_interface.warehouse-service.id,
+  ]
 
-    os_disk {
-        name              = "ansible-node-disk"
-        caching           = "ReadWrite"
-        storage_account_type = "Standard_LRS"
-    }
+  admin_ssh_key {
+    username   = "ansadmin"
+    public_key = file("~/.ssh/id_rsa.pub")
+  }
 
-    source_image_reference {
-        publisher = "OpenLogic"
-        offer     = "CentOS"
-        sku       = "7_8-gen2"
-        version   = "latest"
-    }
+  os_disk {
+    caching              = "ReadWrite"
+    storage_account_type = "Standard_LRS"
+  }
 
-    computer_name  = "ansible-node1"
-    admin_username = "ansadmin"
-    disable_password_authentication = true
-
-    admin_ssh_key {
-        username       = "ansadmin"
-        public_key     = file("~/.ssh/id_rsa.pub")
-    }
-
-    tags = {
-        environment = ""
-    }
+  source_image_reference {
+    publisher = "OpenLogic"
+    offer     = "CentOS"
+    sku       = "7_8-gen2"
+    version   = "latest"
+  }
 }
